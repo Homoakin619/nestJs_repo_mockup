e67fab517a7a47500ecfa9126e9b79747c7d5f68
Azeez Abdulsalam <azeezade04@gmail.com>
2024-07-18 16:02:17 +0100

    fix: updated configs for pm2

--- config/server.config.ts
+++ config/server.config.ts
@@ -4,6 +4,6 @@ dotenv.config()
 
 export default registerAs('server', () => ({
   port: parseInt(process.env.PORT, 10) || 3008,
-  NODE_ENV: process.env.NODE_ENV
-  PROFILE : process.env.PROFILE
+  NODE_ENV: process.env.NODE_ENV,
+  PROFILE : process.env.PROFILE,
 }));
